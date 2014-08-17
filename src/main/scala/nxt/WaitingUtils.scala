b3c2838a788faa9aa53c8d2a522cecd1412adbcf
kushti <kushtech@yahoo.com>
2014-08-17 18:37:41 +0400

    Waiting utils rewritten

--- src/main/scala/nxt/WaitingUtils.scala
+++ src/main/scala/nxt/WaitingUtils.scala
@@ -2,9 +2,32 @@ package nxt.utils
 
 import nxt.{Block, BlockchainListener, Nxt}
 import scala.annotation.tailrec
+import scala.concurrent.{Await, Promise, Future}
+import scala.concurrent.duration._
 
 
 object WaitingUtils {
+
+  //todo: less mutable approach :)
+  lazy val listener = new BlockchainListener {
+    start()
+
+    var tasks = List[(Int,Promise[Unit])]()
+
+    def submit(blocksToWait:Int):Future[Unit] = {
+      val p = Promise[Unit]()
+      tasks = height() + blocksToWait -> p :: tasks
+      p.future
+    }
+
+    def notify(b: Block){
+      tasks.filter(_._1==b.getHeight).foreach{case (_,p)=>
+        p success Unit
+      }
+      tasks = tasks filter(_._1>b.getHeight)
+    }
+  }
+
   def height() = Nxt.getBlockchain.getHeight
 
   def untilSome[T](max: Int)(fn:  => Option[T]): Option[T] = {
@@ -22,27 +45,15 @@ object WaitingUtils {
     step(0)(fn)
   }
 
-  def afterBlock[T](fn:  => Option[T]) = afterNextBlocks(1)(fn)
-
-  //todo: make it non-blocking
-  def afterNextBlocks[T](howMany:Int)(fn: => Option[T]):Option[T] = {
+  def afterNextBlocks[T](howMany:Int)(fn: => T):T = {
     val currentHeight = height()
     println(s"Current height: $currentHeight")
-
-    var res:Option[T] = None
-
-    new BlockchainListener {
-      def notify(b: Block){
-        if(b.getHeight - currentHeight == howMany){
-          println("Block applied: "+b.getHeight)
-          res = fn
-        }
-      }
-    }
-
-    Thread.sleep((howMany+5)*1000) //todo:fix
-    res
+    val f = listener.submit(howMany)
+    Await.result(f, howMany+ 30 seconds)
+    fn
   }
+
+  def afterBlock[T](fn:  => T) = afterNextBlocks(1)(fn)
 
   def skipBlocks(n:Int) = afterNextBlocks(n)(Some())
   def skipBlock() = skipBlocks(1)
